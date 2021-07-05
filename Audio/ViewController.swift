eeb9cc03c0f43211e120506aaddffed95c8c7873
davidyoon <qqpo12@naver.com>
2021-07-05 20:42:32 +0900

    audio layout

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+// 오디오 재생을 위한 헤더
+import AVFoundation
+// 오디오 재생을 위한 델리게이트
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet var pvProgressPlay: UIProgressView!
+    @IBOutlet var lblCurrentTime: UILabel!
+    
+    @IBOutlet var lblEndTime: UILabel!
+    
+    @IBOutlet var btnPlay: UIButton!
+    
+    @IBOutlet var btnPause: UIButton!
+    
+    @IBOutlet var btnStop: UIButton!
+    
+    @IBOutlet var slVolume: UISlider!
+    
+    var audioPlayer: AVAudioPlayer!
+    var audioFile: URL!
+    let MAX_VOLUME: Float = 10.0
+    var progressTimer: Timer!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPlayAudio(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPauseAudio(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func btnStopAudio(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func slChangeVolume(_ sender: UISlider) {
+    }
 }
 
