4e376ce4977df0a77a761f064e4383d92ce71da2
davidyoon <qqpo12@naver.com>
2021-07-05 20:59:55 +0900

    add initPlay func and convertNSTimeInterval2String func

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -34,7 +34,41 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
+        initPlay()
     }
+    
+    // 오디오 재생을 위한 함수
+    func initPlay() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError {
+            print("Error-initPlay: \(error)")
+        }
+        
+        slVolume.maximumValue = MAX_VOLUME // volume slider 맥스값 설정
+        slVolume.value = 1.0 // 볼륨 슬라이더 현재값 설정
+        pvProgressPlay.progress = 0 // 프로그래스뷰 0으로 초기화
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay() // 뭔지 모름
+        audioPlayer.volume = slVolume.value // 오디오 볼륨 슬라이더 값으로 1 설정
+        
+        lblEndTime.text = convertNSTimeInterval2String(audioPlayer.duration) //audio 파일의 총 길이 값 리턴
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
+        
+        
+        
+        
+    }
+    // TimeInterval 값을 받아 문자열로 전환해 주는 함수
+    func convertNSTimeInterval2String(_ time: TimeInterval) -> String {
+        let min = Int(time/60) // 재생시간 매개변수인 time값을 60으로 나눈 '몫'
+        let sec = Int(time.truncatingRemainder(dividingBy: 60)) // time을 60으로 나누고 남은 나머지 값을 sec에 할당
+        let strTime = String(format: "%02d:%02d", min, sec)
+        return strTime
+    }
+    
 
     @IBAction func btnPlayAudio(_ sender: UIButton) {
     }
